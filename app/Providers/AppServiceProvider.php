4d69c51f6f423511489a8a619f28f37c5506c0e4
nmilo <necaneca99@gmail.com>
2023-02-03 15:03:48 +0100

    Add workflows

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -23,6 +23,22 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        if (file_exists(storage_path('workflows/workflows.json'))){
+            $workflows = json_decode(file_get_contents(storage_path('workflows/workflows.json')), true);
+
+            $paths = [];
+
+            foreach ($workflows as $workflow)
+            {
+                $path = $workflow['Path'];
+                if (!in_array($path, $paths))
+                {
+                    $paths[] = $path;
+                }
+            }
+
+            config(['workflows' => $workflows]);
+            config(['paths' => $paths]);
+        }
     }
 }
